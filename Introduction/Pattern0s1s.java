f0c660e4d99ce4a924452a5865bd7f51133584e4
Half_Code <nishantgsingh07@gmail.com>
2018-04-13 08:51:57 +0530

    Initial Commit

---
+++ Introduction/Pattern0s1s.java
@@ -0,0 +1,20 @@
+/*
+Print the pattern 
+0,0
+0,1
+1,0
+1,1
+2,0
+2,1
+*/
+
+
+public class Pattern0s1s {
+  public static void main(String [] args) {
+    for( int x = 0; x<3; x++) {
+      // XXX FOR LOOP MISSING // 
+        System.out.println(x + ", "+y);
+      }
+    }
+  }
+}
